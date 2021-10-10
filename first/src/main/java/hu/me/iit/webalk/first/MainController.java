b55a673288794acad2ac3b0a46f0afbfadff2d73
zumzum00 <eliasdani96@gmail.com>
2021-10-11 00:15:08 +0200

    4het feladat

--- first/src/main/java/hu/me/iit/webalk/first/MainController.java
+++ first/src/main/java/hu/me/iit/webalk/first/MainController.java
@@ -1,6 +1,5 @@
 package hu.me.iit.webalk.first;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -18,56 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/article")
 public class MainController {
-	List<ArticleDto> articles = new ArrayList<>();
+	
+	private final ArticleService articleService;
 
+	public MainController(ArticleService articleService) {
+		super();
+		this.articleService = articleService;
+	}
 	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	List<ArticleDto> allArticles() {
-		return articles;
-	}
-	@GetMapping(path="/{id}")
-	public ArticleDto getArticle(@PathVariable("id") String id) {
-		for (ArticleDto article : articles) {
-			if(article.id == id) {
-				return article;
-			}
-		}
-		return null;
+		return articleService.findAll();
 	}
 
 	@PostMapping(path = "")
-	public void newArticle(@RequestBody ArticleDto articleDto) {
-		articles.add(articleDto);
+	public void newArticle(@RequestBody @Valid ArticleDto articleDto) {
+		articleService.save(articleDto);
 	}
 
-	private int findArticleById(String id) {
-		int found = -1;
-		for (int i = 0; i < articles.size(); i++) {
-			if (articles.get(i).getTitle().equals(id)) {
-				found = i;
-				break;
-			}
-		}
-		return found;
-	}
-
-	@PutMapping(path = "/{id}")
-	public void replaceArticle(@PathVariable("id") String id, @RequestBody  ArticleDto articleDto) {
-
-		int found = findArticleById(id);
-
-		if (found != -1) {
-			ArticleDto foundArticle = articles.get(found);
-			foundArticle.setAuthor(articleDto.getAuthor());
-			foundArticle.setPages(articleDto.getPages());
-		}
+	@PutMapping(path = "/")
+	public void replaceArticle(@RequestBody  ArticleDto articleDto) {
+		articleService.save(articleDto);
 	}
 
 
 	@DeleteMapping(path = "/{id}")
 	public void deleteArticle(@PathVariable("id") String id) {
-		int found = findArticleById(id);
-		if (found != -1) {
-			articles.remove(found);
+
 		}
 	}
-}
